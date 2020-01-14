ed2a6072e02d8e9267f7bda89317fa69b41a88cf
TheBear <44812306+MarcioBBS@users.noreply.github.com>
2020-01-14 09:54:36 -0800

    Set up app controller structure

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// BUDGET CONTROLLER
+var budgetController = (function() {
+   
+
+})();
+
+
+// UI CONTROLLER
+var UIController = (function () {
+
+    // To be defined
+
+})();
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UIctrl) {
+   
+
+})(budgetController, UIController);
