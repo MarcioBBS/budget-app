a995a9f9d5d7e0da69a2e1ab1c3be51a23f91a5b
TheBear <44812306+MarcioBBS@users.noreply.github.com>
2020-01-16 17:18:32 -0800

    add get Input values method - UIController

--- js/app.js
+++ js/app.js
@@ -8,18 +8,37 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function () {
 
-    // To be defined
+   var DOMSTrings = {
+      inputType: '.add__type',
+      inputDescription: '.add__description',
+      inputValue: '.add__value',
+      inputButton: '.add__btn'
+   };
 
+   return {
+      getInput: function() {
+         return {
+            type: document.querySelector(DOMSTrings.inputType).value,
+            description: document.querySelector(DOMSTrings.inputDescription).value,
+            value: document.querySelector(DOMSTrings.inputValue).value
+         }         
+      },
+      getDOMStrings: function() {
+         return DOMSTrings;         
+      }
+   };
 })();
 
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UIctrl) {
 
+   var DOM = UIController.getDOMStrings();
 
    var ctrlAddItem  = function() {
       // Get the field input data
-
+      var values = UIController.getInput();
+      console.log(values);
       // Add the item to the budget controller
 
       // Add the item to the UI
@@ -29,7 +48,7 @@ var controller = (function(budgetCtrl, UIctrl) {
       // Display the budget on the UI            
    }
 
-   document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+   document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
    document.addEventListener('keypress', function(evt) {
       if (evt.keyCode === 13 || evt.which === 13) {
