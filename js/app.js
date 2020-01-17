046b3d2ba9eaff531c7bb38ba595c3c9c8ce2fa7
TheBear <44812306+MarcioBBS@users.noreply.github.com>
2020-01-16 16:00:02 -0800

    Added event listeners

--- js/app.js
+++ js/app.js
@@ -15,6 +15,27 @@ var UIController = (function () {
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UIctrl) {
-   
+
+
+   var ctrlAddItem  = function() {
+      // Get the field input data
+
+      // Add the item to the budget controller
+
+      // Add the item to the UI
+
+      // Calculate the budget
+
+      // Display the budget on the UI            
+   }
+
+   document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+   document.addEventListener('keypress', function(evt) {
+      if (evt.keyCode === 13 || evt.which === 13) {
+         ctrlAddItem();
+      }
+      
+   });
 
 })(budgetController, UIController);
